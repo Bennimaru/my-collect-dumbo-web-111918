70d85c22ac4f021629e29ae1a1a40f7201aaf506
Benson Yu <bennywhyu@gmail.com>
2018-11-14 01:38:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  i=0 
+  new_array=[]
+  
+  while i<array.length 
+    yield array[i]
+    new_array<< (array[i])
+    i= i+1 
+  end 
+  
+ return new_array 
+  
+end 
 
